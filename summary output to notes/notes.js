843334f900f74c3b1112713806daa6230aac19dd
MayankSingamreddy <janus.agent@gmail.com>
2017-07-29 16:03:18 -0700

    Summary to output
    Mayank's files

---
+++ summary output to notes/notes.js
@@ -0,0 +1,20 @@
+
+/*
+var notes  = json.parse(//capi call)
+
+for (var i = 0; i(notes.length; i++) {
+	//process notes[i].title
+	//process notes[i].url
+	//process notes[i].iteration
+
+for var j = 0; j < summary.length; i++{
+	//process notes[i].summary[i]
+}
+}
+*/
+
+for (var i = ; i < notes.length; i++) {
+	var summary = notes[i].summary.split("[BREAK"])
+	for (var j = 0; j < summary.length; j++) {summary[j[]]}
+}
+
