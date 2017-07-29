03d6e64d94687788cfcb31c6f861f9a68bf8f5d1
rooterbuster <roger.luojie.wang@gmail.com>
2017-07-29 16:06:21 -0700

    Extension commit

---
+++ Summ Extension Frontend/content.js
@@ -0,0 +1,12 @@
+
+alert("hey");
+console.log("test");
+var div = document.createElement('div');
+div.style.width = '300px';
+div.style.height = '300px';
+div.style.position = 'fixed';
+div.style.zIndex = '100000';
+div.backgroundColor = 'red';
+
+document.body.appendChild(div);
+
