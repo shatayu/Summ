819fe8a261d6b3f4f3608d80ab07c61615fad2ac
Jaskaran Bakshi <jas10022jb@gmail.com>
2017-07-29 23:04:38 -0700

    added the Get requests
    the state will be changed wen the button is clicked to start recording
    the users data

--- Summ Extension Frontend/content.js
+++ Summ Extension Frontend/content.js
@@ -18,3 +18,34 @@ document.body.appendChild(table);
 
  var html = "<table><tr><td>123</td><td>456</td></tr></table>";
     document.getElementById("table").innerHTML = html;
+
+var state = false
+var link = window.location.href ;
+
+while(state){
+link.onchange = function(){
+	
+var URL_Concept = "hosturl/api/summary/" + link ;
+  $.ajax({
+          type: 'GET',
+          url: URL_Concept,
+          dataType: 'json',
+          success: processJSON,
+          error: function(e)
+          {
+              console.log(e + "sdfsdf")
+          }    
+}
+};
+}
+
+function processJSON(Json){
+
+	var Sentences = JSON.parse(Json);
+	var Links = Sentences.links;
+	document.getElementById("table").innerHTML = Links[0].name + " " + Links[0].url + " " + Links[0].displayUrl + " " + Links[0].description;
+
+	var summary = Sentences.sentences.split("[BREAK]");
+	var title = Sentences.title;
+
+}
