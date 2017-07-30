d907a2908eaf90799a3f7f90687d829757171e52
rooterbuster <roger.luojie.wang@gmail.com>
2017-07-30 08:47:41 -0700

    can navigate through cards!!!

--- Extension Frontend Roger/content.js
+++ Extension Frontend Roger/content.js
@@ -3,7 +3,17 @@ window.onload = function() {
     $.get(chrome.extension.getURL('./overlay.html'), function(data) {
         $(data).appendTo('body');
         //    $($.parseHTML(data)).appendTo('body');
-    });
+    }); 
+    chrome.runtime.onMessage.addListener(
+        function(request, sender, sendResponse) {
+            if( request.message === "start" ) {
+                start();
+            }
+        }
+    );
 
+    function start(){
+        alert("started");
+    }
 
 }
